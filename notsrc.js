60ad03fb49a1676d53d4f8ef11a80b4ebda8ada0
iffy-yeti <118159984+iffy-yeti@users.noreply.github.com>
2023-01-30 18:35:24 +0900

    Create notsrc.js

---
+++ notsrc.js
@@ -0,0 +1,20 @@
+const nestedObj = [     
+    {name: '우준호', 
+phone: "01011111111", 
+tag: ["튜터", "노드"], 
+stack: {node: "backend"}},     
+{name: '김범준', 
+phone: "01011111111", 
+tag: ["튜터", "노드"], 
+stack: {node: "backend"}},
+]
+
+// 함수 만들어서 인자를 배열의 index 로 받고, index 해당하는 사람의 이름과, 그 사람의 stack node (backend) 를 반환하는 함수 만들
+
+//이름 가져오기 ??????
+
+function getTutpr(index) {
+const tutor3 = nestedObj.find((namu) => {
+    return getname.name === "강창민"
+})}
+
